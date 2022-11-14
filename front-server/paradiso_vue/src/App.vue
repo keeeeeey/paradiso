dea47e80f36698a0ee78f0a7e5bc20f5d36a8e48
keeeeeey <sseioul@naver.com>
2022-11-14 20:25:52 +0900

    [FEAT] done auth_vue(login, sighup)

--- front-server/paradiso_vue/src/App.vue
+++ front-server/paradiso_vue/src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="app">
     <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link :to="{ name: 'SignUpView' }">SignUpPage</router-link> | 
+      <router-link :to="{ name: 'LogInView' }">LogInPage</router-link>
     </nav>
     <router-view/>
   </div>
