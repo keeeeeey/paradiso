984428890dc0874d5f0d4adae635d3a5d3b43def
오성훈 <tjd_gns12@naver.com>
2022-11-16 14:16:51 +0900

    done-vue-movieitem

---
+++ final-pjt-front/paradiso_vue/src/components/MovieList.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <h1>영화리스트</h1>
+    <MovieListItem/>
+  </div>
+</template>
+
+<script>
+import MovieListItem from './MovieListItem.vue';
+import axios from 'axios'
+
+export default {
+  name: 'MovieList',
+  components: {
+    MovieListItem,
+  },
+  data() {
+    return {
+      movies: null,
+    }
+  },
+  created() {
+    axios({
+      method: 'get',
+      url: 'http://127.0.0.1:8000/movies/',
+    })
+    .then(res => {
+      console.log(res)
+    })
+    .catch(err => {
+      console.log(err)
+    })
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ final-pjt-front/paradiso_vue/src/components/MovieListItem.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>리스트</h1>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'MovieListItem',
+}
+</script>
+
+<style>
+
+</style>
