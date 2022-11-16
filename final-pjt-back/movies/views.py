acd2010ea71f61d0dc47e54b2727c945c76208bd
keeeeeey <sseioul@naver.com>
2022-11-16 14:43:33 +0900

    [FEAT] 영화 리스트 조회 기능 구현

--- final-pjt-back/movies/views.py
+++ final-pjt-back/movies/views.py
@@ -4,10 +4,16 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
-from .serializers import CommentSerializer, GenreSerializer
-from .models import Comment, Genre
+from .serializers import MovieSerializer, CommentSerializer, GenreSerializer
+from .models import Movie, Comment, Genre
 
 # Create your views here.
+@api_view(['GET'])
+def movie_list(request):
+    movies = get_list_or_404(Movie)
+    serializer = MovieSerializer(movies, many=True)
+    return Response(serializer.data)
+
 @api_view(['GET', 'POST'])
 def comment_list(request, movie_id):
     if request.method == 'GET':
