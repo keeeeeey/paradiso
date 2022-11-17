7e1e91622c977d0b1d098b74b61c69d1adefd9f6
오성훈 <tjd_gns12@naver.com>
2022-11-17 09:11:32 +0900

    done-vue-islike

--- final-pjt-front/paradiso_vue/src/components/MovieListItem.vue
+++ final-pjt-front/paradiso_vue/src/components/MovieListItem.vue
@@ -3,7 +3,8 @@
     <p>제목: {{ movie.title }}</p>
     <p>ID: {{ movie.id }}</p>
     <p>좋아요 개수: {{ likecount }}</p>
-    <button @click="likeMovie">좋아요</button>
+    <button @click="likeMovie" v-show="!islike">좋아요</button>
+    <button @click="likeMovie" v-show="islike">좋아요 취소</button>
   </div>
 </template>
 
@@ -17,7 +18,8 @@ export default {
     },
     data() {
       return {
-        likecount: this.movie.like_users_count
+        likecount: this.movie.like_users_count,
+        islike: false
       }
     },
     methods: {
@@ -45,8 +47,18 @@ export default {
           alert('로그인이 필요합니다')
           this.$router.push({ name: 'LogInView' })
         }
-        
       }
+    },
+    created() {
+      const token = localStorage.getItem('accessToken')
+      axios({
+        method: 'get',
+        url: `http://127.0.0.1:8000/accounts/${this.movie_id}/islike`,
+        headers: {'Authorization': `Bearer ${token}`},
+      })
+      .then((res) => {
+        this.islike = res.data
+      })
     }
 }
 </script>
