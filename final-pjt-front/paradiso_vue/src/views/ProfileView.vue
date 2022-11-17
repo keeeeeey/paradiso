53c76cfa718e67e3b3e3963975b482bc64e42fe2
오성훈 <tjd_gns12@naver.com>
2022-11-17 15:21:39 +0900

    fix-vue-profile

--- final-pjt-front/paradiso_vue/src/views/ProfileView.vue
+++ final-pjt-front/paradiso_vue/src/views/ProfileView.vue
@@ -2,8 +2,7 @@
   <div>
     <h1>Profile 입니다.</h1>
     {{ nickname }}
-    {{ totaldata.serializer.isFollowd }}
-    팔로우: {{ totaldata }}
+    {{ userData }}
     <button @click="follow" v-show="!isfollow">팔로우</button>
     <button @click="follow" v-show="isfollow">언팔로우</button>
   </div>
@@ -22,6 +21,11 @@ export default {
         isfollow: false,
       }
     },
+    computed: {
+      userData() {
+        return this.totaldata.userSerializer
+      }
+    },
     created() {
       const token = localStorage.getItem('accessToken')
       axios({
@@ -29,14 +33,13 @@ export default {
         url: `http://127.0.0.1:8000/accounts/profile/${this.nickname}/`,
       })
       .then(res => {
-        console.log(res.data)
-        this.totaldata = res.data
+        this.totaldata = res.data.serializer
       })
       if (token) {
         axios({
           method: 'get',
           url: `http://127.0.0.1:8000/accounts/${this.nickname}/isfollow/`,
-          headers: {'Authorization': `Bearer ${token}`},
+          headers: {'Authorization': `Bearer ${token}`}
         })
         .then(res => {
           this.isfollow = res.data.is_followed
@@ -48,12 +51,14 @@ export default {
     },
     methods: {
       follow() {
+        const token = localStorage.getItem('accessToken')
         axios({
           method: 'post',
-          url: `http://127.0.0.1:8000/accounts/${this.totaldata}/follow/`
+          url: `http://127.0.0.1:8000/accounts/${this}/follow/`,
+          headers: {'Authorization': `Bearer ${token}`}
         })
         .then(() => {
-          
+          console.log('팔로우 성공')
         })
       }
     }
