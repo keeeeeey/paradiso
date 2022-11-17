efb8c1a6a338d2ec0118d75f51027602e5026f7a
오성훈 <tjd_gns12@naver.com>
2022-11-17 15:04:01 +0900

    fix vue-profile

--- final-pjt-front/paradiso_vue/src/views/ProfileView.vue
+++ final-pjt-front/paradiso_vue/src/views/ProfileView.vue
@@ -3,8 +3,9 @@
     <h1>Profile 입니다.</h1>
     {{ nickname }}
     {{ totaldata.serializer.isFollowd }}
-    <!-- <button @click="follow" v-show="!isfollow">팔로우</button>
-    <button @click="follow" v-show="isfollow">언팔로우</button> -->
+    팔로우: {{ totaldata }}
+    <button @click="follow" v-show="!isfollow">팔로우</button>
+    <button @click="follow" v-show="isfollow">언팔로우</button>
   </div>
 </template>
 
@@ -22,14 +23,28 @@ export default {
       }
     },
     created() {
+      const token = localStorage.getItem('accessToken')
       axios({
         method: 'get',
-        url: `http://127.0.0.1:8000/accounts/profile/${this.nickname}/`
+        url: `http://127.0.0.1:8000/accounts/profile/${this.nickname}/`,
       })
       .then(res => {
         console.log(res.data)
         this.totaldata = res.data
       })
+      if (token) {
+        axios({
+          method: 'get',
+          url: `http://127.0.0.1:8000/accounts/${this.nickname}/isfollow/`,
+          headers: {'Authorization': `Bearer ${token}`},
+        })
+        .then(res => {
+          this.isfollow = res.data.is_followed
+        })
+      } else {
+        this.isfollow = true
+      }
+  
     },
     methods: {
       follow() {
