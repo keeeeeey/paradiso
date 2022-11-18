4eb8069c8b6e558791419cae0252152bea127be9
OctoHun <97264011+OctoHun@users.noreply.github.com>
2022-11-18 12:48:43 +0900

    vue-css-fix

--- final-pjt-front/paradiso_vue/src/components/NowPlayList.vue
+++ final-pjt-front/paradiso_vue/src/components/NowPlayList.vue
@@ -1,21 +1,32 @@
 <template>
   <div id="now-play-list">
-    <h1>NowPlayList</h1>
+    <NowPlayListItem v-for="nowplay in playlist" :key="nowplay.id" :playmovie="nowplay"/>
   </div>
 </template>
 
 <script>
 import axios from "axios"
+import NowPlayListItem from '@/components/NowPlayListItem'
+import _ from 'lodash'
 
 export default {
     name: "NowPlayList",
+    data() {
+      return {
+        playlist: null,
+      }
+    },
+    components: {
+      NowPlayListItem,
+    },
     created() {
         axios({
             method: "get",
             url: "https://api.themoviedb.org/3/movie/now_playing?api_key=fc9c1f07b9650d1ec4f98fb39efa792e&language=ko-KR&page=1"
         })
             .then((res) => {
-                console.log(res.data.results)
+              console.log(res.data)
+                this.playlist = _.sampleSize(res.data.results, 5)
             })
     }
 }
---
+++ final-pjt-front/paradiso_vue/src/components/NowPlayListItem.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    {{ playmovie.title }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NowPlayListItem',
+  props: {
+    playmovie: Object,
+  }
+}
+</script>
+
+<style>
+
+</style>
