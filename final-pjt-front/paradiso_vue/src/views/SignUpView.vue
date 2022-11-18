e7642e6d1505b4d8b42d79f9a82d78b4709c2b66
keeeeeey <sseioul@naver.com>
2022-11-18 16:37:20 +0900

    [FEAT] 아이디 체크 기능 구현

--- final-pjt-front/paradiso_vue/src/views/SignUpView.vue
+++ final-pjt-front/paradiso_vue/src/views/SignUpView.vue
@@ -1,23 +1,37 @@
 <template>
-    <div id="sign-up-view" class="margin-by-fixed">
-      <h1>Sign Up Page</h1>
+    <div id="sign-up-view" class="margin-by-fixed w-25 m-auto">
+      <h1 class="mt-5">회원가입</h1>
       <form @submit.prevent="signUp">
-        <label for="username">username : </label>
-        <input type="text" id="username" v-model="username"><br>
+        <div class="mb-3">
+          <label for="username" class="form-label">아이디</label>
+          <input type="text" class="form-control" id="username" aria-describedby="emailHelp" v-model.trim="username" @blur="idCheck">
+          <div id="idMessage" class="form-text"></div>
+        </div>
 
-        <label for="email">email : </label>
-        <input type="text" id="email" v-model="email"><br>
-  
-        <label for="nickname">nickname : </label>
-        <input type="text" id="nickname" v-model="nickname"><br>
+        <div class="mb-3">
+          <label for="email" class="form-label">이메일</label>
+          <input type="email" class="form-control" id="email" v-model="email">
+        </div>
 
-        <label for="password"> password : </label>
-        <input type="password" id="password" v-model="password"><br>
+        <div class="mb-3">
+          <label for="nickname" class="form-label">닉네임</label>
+          <input type="text" class="form-control" id="nickname" v-model="nickname">
+        </div>
 
-        <label for="passwordConfirm"> password confirmation : </label>
-        <input type="password" id="passwordConfirm" v-model="passwordConfirm">
-        
-        <input type="submit" value="SignUp">
+        <div class="mb-3">
+          <label for="password" class="form-label">비밀번호</label>
+          <input type="password" class="form-control" id="password" v-model="password">
+        </div>
+
+        <div class="mb-3">
+          <label for="passwordConfirm" class="form-label">비밀번호 확인</label>
+          <input type="password" class="form-control" id="passwordConfirm" v-model="passwordConfirm">
+        </div>
+        <!-- <div class="mb-3 form-check">
+          <input type="checkbox" class="form-check-input" id="exampleCheck1">
+          <label class="form-check-label" for="exampleCheck1">Check me out</label>
+        </div> -->
+        <button type="submit" class="btn btn-primary">회원가입</button>
       </form>
     </div>
 </template>
@@ -56,6 +70,31 @@ export default {
           this.$router.push({ name: "LogInView" })
         })
         .catch((err) => console.log(err))
+    },
+
+    idCheck() {
+      const username = this.username
+
+      const message = document.querySelector("#idMessage")
+      if (!username) {
+        message.innerText = "아이디를 입력해주세요."
+        message.style.color = "red"
+        return
+      }
+
+      axios({
+        method: "get",
+        url: `${API_URL}/accounts/idcheck/${username}`,
+      })
+        .then((res) => {
+          if (res.data.id_exist) {
+            message.innerText = "이미 존재하는 아이디입니다."
+            message.style.color = "red"
+          } else {
+            message.innerText = "사용 가능한 아이디입니다."
+            message.style.color = "green"
+          }
+        })
     }
   }
 }
