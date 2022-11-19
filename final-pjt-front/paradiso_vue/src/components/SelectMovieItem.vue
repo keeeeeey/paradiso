34f8adaa3e9f5753d946a3435c8477d652496b4e
keeeeeey <sseioul@naver.com>
2022-11-19 22:07:40 +0900

    [FIX] 좋아하는 영화 선택 페이지

---
+++ final-pjt-front/paradiso_vue/src/components/SelectMovieItem.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="select-movie-box" :class="{ 'checked-box' : isChecked }">
+    <img :src="IMG_URL + movie.poster_path" alt="" class="mw-100" style="height: 100%" @click="selectFavoriteMovie(movie.id)" :class="{checked : isChecked}">
+  </div>
+</template>
+
+<script>
+
+export default {
+    name: "SelectMovieView",
+    props: {
+        movie: Object
+    },
+    data() {
+        return {
+            IMG_URL: "https://image.tmdb.org/t/p/original",
+            isChecked: false,
+        }
+    },
+    methods: {
+      selectFavoriteMovie(id) {
+        this.isChecked = !this.isChecked
+        this.$emit("from-select-movie-item", id)
+      }
+    },
+}
+</script>
+
+<style>
+.checked-box {
+    box-shadow: 0 0 .2rem #fff, 0 0 .2rem #fff, 0 0 2rem #1f87c3, 0 0 0.8rem #1f87c3, 0 0 2.8rem #1f87c3;
+    z-index: 1;
+}
+
+.checked {
+  scale: 1.1;
+}
+
+.select-movie-box {
+  overflow: hidden;
+  border-radius: 10px;
+}
+
+.select-movie-box img {
+  transition: all 0.2s linear;
+}
+
+</style>
--- final-pjt-front/paradiso_vue/src/views/SelectMovieView.vue
+++ final-pjt-front/paradiso_vue/src/views/SelectMovieView.vue
@@ -1,37 +1,47 @@
 <template>
   <div id="select-movie-view" class="margin-by-fixed">
     <h1>SelectMovieView</h1>
-    <form @submit.prevent="submitFavorite" class="row">
-      <div v-for="movie in movies" :key="movie.id" class="p-0 col-2">
-        <!-- <input type='checkbox' :name='movie.id' :value='movie.id' />
-        {{ movie.title }} -->
-        <img :src="API_URL + movie.poster_path" alt="" class="mw-100" style="height: 100%" @click="selectFavoriteMovie(movie.id)">
+    <form @submit.prevent="submitFavorite">
+      <input type="text" class="btn btn-primary" value="선택완료">
+      <div class="row">
+        <SelectMovieItem v-for="movie in movies" :key="movie.id" :movie=movie class="p-0 col-2" @from-select-movie-item=fromSelectMovieItem />
       </div>
     </form>
   </div>
 </template>
 
 <script>
+import SelectMovieItem from "@/components/SelectMovieItem"
 import axios from "axios"
 const API_URL = "http://127.0.0.1:8000"
 
 export default {
     name: "SelectMovieView",
+    components: {
+      SelectMovieItem
+    },
     data() {
       return {
         movies: null,
-        API_URL: "https://image.tmdb.org/t/p/original",
         my_favorite_movies: []
       }
     },
     methods: {
-      selectFavoriteMovie(id) {
+      fromSelectMovieItem(id) {
         if (this.my_favorite_movies.length === 10) {
-          alert("영화는 ")
-        } 
+          alert("영화 선택은 10개 까지만 가능합니다.")
+        }
+
+        if (this.my_favorite_movies.includes(id)) {
+          const idx = this.my_favorite_movies.indexOf(id)
+          this.my_favorite_movies.splice(idx, 1)
+          console.log(this.my_favorite_movies)
+        } else {
+          this.my_favorite_movies.push(id)
+          console.log(this.my_favorite_movies)
+        }
+
         
-        this.my_favorite_movies.push(id)
-        console.log(this.my_favorite_movies)
       }
     },
     created() {
