ce0cefe75e136112054da43869225f84d0c6bf8b
OctoHun <97264011+OctoHun@users.noreply.github.com>
2022-11-19 20:38:17 +0900

    vue-fix

--- final-pjt-front/paradiso_vue/src/views/SelectMovieView.vue
+++ final-pjt-front/paradiso_vue/src/views/SelectMovieView.vue
@@ -1,6 +1,13 @@
 <template>
   <div id="select-movie-view" class="margin-by-fixed">
     <h1>SelectMovieView</h1>
+    <form @submit.prevent="submitFavorite" class="row">
+      <div v-for="movie in movies" :key="movie.id" class="p-0 col-2">
+        <!-- <input type='checkbox' :name='movie.id' :value='movie.id' />
+        {{ movie.title }} -->
+        <img :src="API_URL + movie.poster_path" alt="" class="mw-100" style="height: 100%" @click="selectFavoriteMovie(movie.id)">
+      </div>
+    </form>
   </div>
 </template>
 
@@ -10,6 +17,23 @@ const API_URL = "http://127.0.0.1:8000"
 
 export default {
     name: "SelectMovieView",
+    data() {
+      return {
+        movies: null,
+        API_URL: "https://image.tmdb.org/t/p/original",
+        my_favorite_movies: []
+      }
+    },
+    methods: {
+      selectFavoriteMovie(id) {
+        if (this.my_favorite_movies.length === 10) {
+          alert("영화는 ")
+        } 
+        
+        this.my_favorite_movies.push(id)
+        console.log(this.my_favorite_movies)
+      }
+    },
     created() {
       axios({
         method: "get",
@@ -17,6 +41,7 @@ export default {
       })
         .then((res) => {
           console.log(res.data)
+          this.movies = res.data
         })
         .catch((err) => {
           console.log(err)
