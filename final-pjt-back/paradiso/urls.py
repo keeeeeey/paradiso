2c15f03bf3951eda993a139cd4393967be3d321f
keeeeeey <sseioul@naver.com>
2022-11-22 17:07:03 +0900

    [FEAT] refresh 토큰 적용

--- final-pjt-back/paradiso/urls.py
+++ final-pjt-back/paradiso/urls.py
@@ -1,12 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
-# from rest_framework_simplejwt.views import TokenObtainPairView
 from accounts.serializers import MyTokenObtainPairView
+from rest_framework_simplejwt.views import TokenRefreshView
+from rest_framework_simplejwt.views import TokenVerifyView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('accounts/', include("accounts.urls")),
     path('movies/', include("movies.urls")),
     path('accounts/', include('accounts.urls')),
-    path("api/token/", MyTokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/token/", MyTokenObtainPairView.as_view()),
+    path('api/token/refresh/', TokenRefreshView.as_view()),
+    path("api/token/verify/", TokenVerifyView.as_view()),
 ]
