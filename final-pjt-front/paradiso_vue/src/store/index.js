37f7b2cbc5a2ebbaf273b11c183fc857ee02ea7d
keeeeeey <sseioul@naver.com>
2022-11-23 09:47:15 +0900

    [CHORE] 코드 정리

--- final-pjt-front/paradiso_vue/src/store/index.js
+++ final-pjt-front/paradiso_vue/src/store/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import createPersistedState from 'vuex-persistedstate'
+import axios from 'axios'
 
 Vue.use(Vuex)
 
@@ -29,6 +30,25 @@ export default new Vuex.Store({
 
     delete_user(context) {
       context.commit("DELETE_USER")
+    },
+
+    refresh() {
+      const refreshToken = localStorage.getItem("refreshToken")
+
+      axios({
+        method: "post",
+        url: "http://127.0.0.1:8000/api/token/refresh/",
+        data: {'refresh': refreshToken},
+      })
+        .then((res) => {
+          localStorage.setItem("accessToken", res.data.access)
+          this.isLoggedIn = true;
+        })
+        .catch(() => {
+          localStorage.removeItem("accessToken")
+          localStorage.removeItem("refreshToken")
+          this.$router.push({ name: "LogInView" })
+        })
     }
   },
   modules: {
